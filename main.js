57245b0c917044d184ee987f48d7a56455d7f4c6
ddjin <deniseduong644@gmail.com>
2025-10-27 18:45:53 -0700

    i dont get it hour two

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // connect the js elements to the html elements
 const input = document.getElementById('input');
 
+// define canvas variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
+// canvas methods 
+
+
 // create web audio api elements
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
@@ -17,6 +26,11 @@ gainNode.gain.value = 0;
 
 //allow users to type in a key, shorthand for the frequencies
 notenames = new Map();
+
+//variable that represents the corresponding frequency to the user input 
+
+
+// shorthands for frequencies
 notenames.set("C", 261.6);
 notenames.set("D", 293.7);
 notenames.set("E", 329.6);
@@ -24,10 +38,6 @@ notenames.set("F", 349.2);
 notenames.set("G", 392.0);
 notenames.set("A", 440);
 notenames.set("B", 493.9);
-
-//variable that represents the corresponding frequency to the user input 
-var usernotes = String(input.value);
-frequency(notenames.get(usernotes));
 
 // function to set frequency and channel sound
 function frequency(pitch){
@@ -38,9 +48,10 @@ gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
 
 // function to handle button click and stop and resume
 function handle() { 
+var usernotes = String(input.value);
  audioCtx.resume();
  gainNode.gain.value = 0;
-frequency(input.value);
+ frequency(notenames.get(usernotes));
 }
 
 
