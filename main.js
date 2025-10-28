a9851e1d7cde889eb7799c69ca5942258956067e
ddjin <deniseduong644@gmail.com>
2025-10-27 17:47:30 -0700

    hour one

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
+// connect the js elements to the html elements
 const input = document.getElementById('input');
-
-function frequency(pitch){
-
-
-}
-
-function handle() { 
- audioCtx.resume();
- gainNode.gain.value = 0;
-    frequency(input.value);
- 
-}
 
 // create web audio api elements
 const audioCtx = new AudioContext();
@@ -22,5 +11,38 @@ oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type ="sine";
 
+// make the oscillator not start immediately
 oscillator.start();
 gainNode.gain.value = 0;
+
+//allow users to type in a key, shorthand for the frequencies
+notenames = new Map();
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440);
+notenames.set("B", 493.9);
+
+//variable that represents the corresponding frequency to the user input 
+var usernotes = String(input.value);
+frequency(notenames.get(usernotes));
+
+// function to set frequency and channel sound
+function frequency(pitch){
+gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+}
+
+// function to handle button click and stop and resume
+function handle() { 
+ audioCtx.resume();
+ gainNode.gain.value = 0;
+frequency(input.value);
+}
+
+
+
+
